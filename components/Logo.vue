73d142141bcd0099af39de404bd0c5f795d55d76
Isaacddr <isaacovic00@gmail.com>
2021-10-18 23:59:14 -0500

    chore: first push

---
+++ components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <nuxt-img src="/img/mariabonita.png" class="w-1/2" />
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-gray-900">
+    <Nav />
+    <Nuxt />
+  </div>
+</template>
