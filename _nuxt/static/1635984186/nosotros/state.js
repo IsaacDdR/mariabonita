efa94ac0f089d21102ecd2dd2c083e2b244bd52b
IsaacDdR <IsaacDdR@users.noreply.github.com>
2021-11-16 18:27:34 +0000

    deploy: 497d8bd332ec582dd9716a98161abe67d4b37ffa

--- _nuxt/static/1635984186/nosotros/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmariabonita\u002F_nuxt\u002Fstatic\u002F1635984186",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnosotros",config:{_app:{basePath:"\u002Fmariabonita\u002F",assetsPath:"\u002Fmariabonita\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1637087251/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontacto","\u002Fnosotros","\u002Ftestimonios","\u002Fservicios"]})
